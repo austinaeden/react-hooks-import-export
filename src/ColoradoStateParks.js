ac3eb9a7e941e74c035766f05ba19a7de68c67b6
austinaeden <austin.mbogo@student.moringaschool.com>
2023-07-20 07:22:27 +0300

    completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,20 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  RMFunctions.wildlife();//=> "Elk, Bighorn Sheep, Moose"
+  console.log(RMFunctions.trees);
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <MesaVerde/>
+      <h1>Colorado State Parks!</h1>
+    </div>
+
+  );
 }
+
+export default ColoradoStateParks;
